1f92876ea18c12b86a4e3eeb6e79f81770c9e3e3
William 'Q' Jones <williamjonesfbisd@gmail.com>
2024-11-20 08:02:24 -0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+    "./src/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
